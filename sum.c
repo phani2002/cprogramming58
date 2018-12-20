cda7bccbf2142094481e2c976d67e12aac528c98
phani2002 <46022835+phani2002@users.noreply.github.com>
2018-12-20 17:45:30 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("enter the value of b");
+scanf("%d",&b);
+c=a+b;
+printf("the value of c is ",c);
+getch();
+}
